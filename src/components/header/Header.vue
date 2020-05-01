1aa34505715ef75fd0945b3f27db3e587a54450f
Gustavo Passos <theguspassos@gmail.com>
2020-04-30 22:23:30 -0300

    remove unecessary components

---
+++ src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>test</header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Header"
+});
+</script>
+
+<style scoped>
+</style>
