f9d07e19f561ac1681a11aba1bdd84b9fc3d0f96
Gustavo Passos <theguspassos@gmail.com>
2020-05-04 09:49:39 -0300

    change header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -3,13 +3,19 @@
     <div class="title">
       <span>AcaDev Blog</span><span>by Gustavo Passos</span>
     </div>
+    <MenuIcon></MenuIcon>
   </header>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import MenuIcon from "./MenuIcon.vue";
+
 export default Vue.extend({
   name: "Header",
+  components: {
+    MenuIcon,
+  },
 });
 </script>
 
@@ -20,9 +26,12 @@ header {
   background: $main-color;
   color: $main-text-color;
   padding: 4% 8%;
+  display: flex;
 }
 
 .title {
+  width: 60%;
+
   span {
     display: block;
   }
---
+++ src/components/header/MenuIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hamburger-icon">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "MenuIcon",
+});
+</script>
+
+<style lang="scss" scoped>
+.hamburger-icon {
+  width: 40px;
+  float: right;
+
+  div {
+    background: #fff;
+    width: 100%;
+    height: 4px;
+    margin: 7px 0;
+  }
+}
+</style>
