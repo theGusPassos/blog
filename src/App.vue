d14fd15aa965ac494c8aed50d9b001fca0da42db
Gustavo Passos <theguspassos@gmail.com>
2020-05-04 18:16:05 -0300

    add sample blog page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
     <Header></Header>
+    <div class="router-body">
+      <Post />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Header from "@/components/header/Header.vue";
+import Post from "@/components/post/Post.vue";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "App",
   components: {
     Header,
-  },
+    Post
+  }
 });
 </script>
 
@@ -23,5 +28,9 @@ body {
   height: 100%;
   margin: 0;
   font-family: "Quicksand";
+}
+
+.router-body {
+  padding: 3% 8% 0 8%;
 }
 </style>
---
+++ src/components/post/Post.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <img src="../../static/post.jpg" alt="jumping" />
+    <h1>2D animation in Unity with different sprite sizes</h1>
+    <div class="post-info">
+      <span>Gustavo Passos</span>
+      <span>25/04/2020</span>
+    </div>
+
+    <section>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nisi vitae odio scelerisque, eu viverra nulla pharetra. Quisque eu ultrices sapien. Maecenas eget tempor arcu.</p>
+      <p>Nullam rutrum erat a ante dapibus egestas. Mauris eu nisi sed eros dictum fermentum. Nullam finibus, sapien vulputate pellentesque dignissim, sapien ex feugiat tellus, in euismod turpis sem pellentesque orci. Maecenas placerat ullamcorper neque.</p>
+    </section>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Post"
+});
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 0;
+}
+
+img {
+  width: 100%;
+  border-radius: 10px;
+}
+
+.post-info {
+  margin: 10px 0px;
+
+  span {
+    display: block;
+  }
+}
+
+p {
+  font-size: 1.1em;
+  line-height: 1.5;
+}
+</style>
