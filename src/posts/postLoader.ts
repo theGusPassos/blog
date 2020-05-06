fcc1039a2a9e3ea76b1f9d09a54ce259056a749f
Gustavo Passos <theguspassos@gmail.com>
2020-05-06 18:45:01 -0300

    set async fetch of assets for posts

--- src/posts/postLoader.ts
+++ src/posts/postLoader.ts
@@ -1,6 +1,4 @@
-import pos from "@/static/2D_animation_in_unity_with_different_sprite_sizes/post.md";
-
 export const getPosts = () => {
-  console.log(pos);
+  fetch("posts/1_2D_unity/post.md").then((a) => console.log(a));
   return "null";
 };
