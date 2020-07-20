099d8b509bd121e5a6c45807cb547f056c7a4300
Gustavo Passos <theguspassos@gmail.com>
2020-07-20 19:18:52 -0300

    add credits to acadev logo

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,21 +1,28 @@
 <template>
-  <section>
-    <p>
-      This blog porpuse is to share knowledge that might be useful for other
-      <strong>software and game developers</strong>.
-    </p>
-    <p>
-      I have a
-      <a
-        href="https://theguspassos.github.io/"
-        target="_blank"
-        rel="noreferrer nofollow"
-        >personal website</a
-      >
-      with my contacts and portfolio if you’re interested.
-    </p>
-    <p>Feel free to get in touch =)</p>
-  </section>
+  <div>
+    <section>
+      <p>
+        This blog porpuse is to share knowledge that might be useful for other
+        <strong>software and game developers</strong>.
+      </p>
+      <p>
+        I have a
+        <a
+          href="https://theguspassos.github.io/"
+          target="_blank"
+          rel="noreferrer nofollow"
+        >personal website</a>
+        with my contacts and portfolio if you’re interested.
+      </p>
+      <p>Feel free to get in touch =)</p>
+    </section>
+    <section>
+      <p>
+        this website logo was made by
+        <a href="https://www.instagram.com/xkobita/">xkobita</a>
+      </p>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
@@ -25,15 +32,18 @@ export default Vue.extend({
   name: "About",
   created() {
     this.$ga.page(this.$router);
-  },
+  }
 });
 </script>
 
 <style lang="scss" scoped>
 @import "../../styles/colors.scss";
 
+section {
+  padding: 40px 0;
+}
+
 p {
   font-size: 1.2em;
-  padding: 5px 0;
 }
 </style>
