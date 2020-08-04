c1ae75250e23956147702dc9bf39a8175593dbb2
Gustavo Passos <theguspassos@gmail.com>
2020-08-04 19:11:27 -0300

    reorganize list

--- src/data/posts/posts.ts
+++ src/data/posts/posts.ts
@@ -2,11 +2,11 @@ import Post from "@/models/post";
 
 export const posts: Post[] = [
   {
-    id: 1,
-    date: new Date(2020, 4, 24),
-    title: "2D animation in Unity with different sprite sizes",
-    folder: "1_2d_animation_in_unity_with_different_sprite_sizes",
-    issueId: 2,
+    id: 3,
+    date: new Date(2020, 8, 4),
+    title: "My experience in the 2020 GMTK Game Jam",
+    folder: "3_my_experience_in_the_2020_GMTK_game_jam",
+    issueId: 7,
   },
   {
     id: 2,
@@ -16,10 +16,10 @@ export const posts: Post[] = [
     issueId: 4,
   },
   {
-    id: 3,
-    date: new Date(2020, 8, 4),
-    title: "My experience in the 2020 GMTK Game Jam",
-    folder: "3_my_experience_in_the_2020_GMTK_game_jam",
-    issueId: 7,
+    id: 1,
+    date: new Date(2020, 4, 24),
+    title: "2D animation in Unity with different sprite sizes",
+    folder: "1_2d_animation_in_unity_with_different_sprite_sizes",
+    issueId: 2,
   },
 ];
